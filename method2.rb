7bf237060e2b7c13d66fb482e7daadfd84370684
Rohit <rohitessapur@gmail.com>
2023-04-06 21:46:09 +0530

    modofied

--- method2.rb
+++ method2.rb
@@ -14,3 +14,22 @@ def test
     yield
  end
  test {puts "You are in the block"}
+
+ 
+ class Name                         # without arguments
+    def rr 
+        puts "hello to methods"
+    end
+end
+new = Name.new
+new.rr
+#with arguments
+
+class Exp 
+    def ss sum
+        puts "we can do the #{sum} on many points"
+    end
+end
+
+object = Exp.new
+object.ss("multiplication")
