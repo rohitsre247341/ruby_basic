b486d3be65575d5ab881d5cfd47ec563e764e9bd
Rohit <rohitessapur@gmail.com>
2023-04-07 10:34:22 +0530

    sum of two numbers

---
+++ sum_of_two_numbers.rb
@@ -0,0 +1,12 @@
+puts "           welcome"
+puts "we are doing sum of two given numbers"
+s =2 
+z =45
+
+def sum(a,b)
+    sume = a+b
+ 
+end
+    var = sum(s,z)
+     puts "     first number is #{s} and second number is #{z}"
+    puts "  sum of these two given numbers    =      #{var}   "
