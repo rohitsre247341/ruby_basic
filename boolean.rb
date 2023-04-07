7e541e11e92f5476543321b0a80909c3e0fef5d2
Rohit <rohitessapur@gmail.com>
2023-04-07 18:24:12 +0530

    boolean expression programe

---
+++ boolean.rb
@@ -0,0 +1,15 @@
+# for checking male and tall status using boolean 
+
+male = true
+tall = true
+
+if male and tall 
+   puts "you are a tall man"
+elsif male and !tall
+   puts "you are a short male"
+elsif !male and tall
+   puts "you are not male but tall "
+else 
+   puts "you are not and not tall"
+      
+   end
